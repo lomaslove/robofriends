a8098dfa29bfaee4257094a0ac320b5c7fa1c215
rails course from udemy <lomaslove132@gmail.com>
2019-12-11 21:50:04 +1000

    Updates

---
+++ precache-manifest.c84f7df96b9feabc82988b998c4db54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4366b1b68110c1059705829b996c13cb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d41a4f40930e37189b13",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ad2784537e12e4d6b793",
+    "url": "/robofriends/static/css/main.c30565cd.chunk.css"
+  },
+  {
+    "revision": "d41a4f40930e37189b13",
+    "url": "/robofriends/static/js/2.85c4fb15.chunk.js"
+  },
+  {
+    "revision": "ad2784537e12e4d6b793",
+    "url": "/robofriends/static/js/main.04f876f1.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  }
+]);
