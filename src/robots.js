880851c3c6fd911ebf16e32adb3af4a43f7cd196
rails course from udemy <lomaslove132@gmail.com>
2019-12-12 21:44:38 +1000

    update folder

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Yubin Bista',
+	username: 'Yubi',
+	email: 'YubinBista@gmail.com'
+},
+
+{
+	id: 2,
+	name: 'Pratima Baral',
+	username: 'Musarni',
+	email: 'musarni@gmail.com'
+},
+
+{
+	id: 3,
+	name: 'Toyota Corolla',
+	username: 'Ascent',
+	email: 'tiyotacorolla@gmail.com'
+},
+
+{
+	id: 4,
+	name: 'Mazda Azami',
+	username: 'Cx9',
+	email: 'mazdazami@gmail.com'
+},
+
+{
+	id: 5,
+	name: 'Yubin Bista',
+	username: 'Yubi',
+	email: 'YubinBista@gmail.com'
+},
+{
+	id: 6,
+	name: 'Pratima Baral',
+	username: 'Musarni',
+	email: 'musarni@gmail.com'
+},
+
+{
+	id: 7,
+	name: 'Mazda Azami',
+	username: 'Cx9',
+	email: 'mazdazami@gmail.com'
+},
+
+{
+	id: 8,
+	name: 'Pratima Baral',
+	username: 'Musarni',
+	email: 'musarni@gmail.com'
+},
+]
